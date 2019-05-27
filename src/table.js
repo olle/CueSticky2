d27a49f9fad8b5c8a04614f98f02a0e7cf800801
Olle Törnström <toernstroem@synyx.de>
2019-05-27 17:47:11 +0200

    Add table docs

--- src/table.js
+++ src/table.js
@@ -1,10 +1,33 @@
 /**
  * A pool table.
+ *
+ * @module table
  */
 import * as pocket from "./pocket";
 
+/**
+ * A pool table data object implementation, providing a simple representation.
+ *
+ * @example
+ *
+ *  {
+ *     pockets: []
+ *  }
+ *
+ * @typedef {Object} Table
+ * @property {array} pockets - the table pockets
+ */
+
+/**
+ * Typical pool table wall dimension (width/height).
+ */
 const WALL_WIDTH_N_HEIGHT = 0.05;
 
+/**
+ * Creates a new pool table data object instance.
+ *
+ * @returns {Table} a new pool table object instance, with 6 pockets
+ */
 export function neu() {
   let dim = WALL_WIDTH_N_HEIGHT;
   let pad = WALL_WIDTH_N_HEIGHT / 3;
